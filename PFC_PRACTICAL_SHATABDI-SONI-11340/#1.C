2d25281dfe4af59c1d4d92acaca842f27bb05394
shatabdisoni-ui <shatabdi.soni@rungta.org>
2026-01-12 13:33:28 +0530

    //

---
+++ PFC_PRACTICAL_SHATABDI-SONI-11340/#1.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Variable declaration
+    char name[] = "Soumya Soni";
+    int age = 22;
+
+    // Output statements
+    printf("Hello, World!\n");
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+
+    return 0;  // End of program
+}
